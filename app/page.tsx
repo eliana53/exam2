95390c4a89d4c3967f0cbab9c41732ed0e7294fd
eliana53 <elianafurman1@gmail.com>
2024-12-10 00:03:37 +0000

    added date

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-4">
         Welcome to my Chinese Restaurant
       </h1>
+      Date: 12/9/2024
+      
       <p className="text-lg">Enjoy our chinese food</p>
     </div>
   );
