8e26155b5f041a913eaa53039480de4ac2d4f8ae
eliana53 <elianafurman1@gmail.com>
2024-12-15 23:47:44 +0000

    Golden Woc

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">
-        Welcome to my Chinese Restaurant
+        Welcome to Golden Dragon Wock
       </h1>
       Date: 12/9/2024
       
-      <p className="text-lg">Enjoy our chinese food</p>
+      <p className="text-lg">Enjoy delicious chinese food</p>
     </div>
   );
 }
